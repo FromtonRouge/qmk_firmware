8322e1a26e579f30186378e69b1172129f8ffadc
FromtonRouge <fromtonrouge@gmail.com>
2017-04-20 22:11:34 +0200

    Build script updated

--- keyboards/ergodox/keymaps/fromtonrouge/firmwares/build.sh
+++ keyboards/ergodox/keymaps/fromtonrouge/firmwares/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd ../../../../..
+rm -rf .build
 
 # ErgoDox EZ binaries
 
@@ -14,19 +15,15 @@ cp ergodox_ez_fromtonrouge.hex "$OLDPWD/ez"/ez-qwerty-jackdaw-qwerty.hex
 
 # Infinity ErgoDox binaries
 
-make ergodox-infinity-fromtonrouge-dfu-util PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=yes
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-colemak-jackdaw-azerty-left.bin
-make ergodox-infinity-fromtonrouge-dfu-util MASTER=right PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=yes
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-colemak-jackdaw-azerty-right.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=yes .build/infinity-colemak-jackdaw-azerty-left.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge MASTER=right PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=yes .build/infinity-colemak-jackdaw-azerty-right.bin
 
-make ergodox-infinity-fromtonrouge-dfu-util PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=no
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-colemak-jackdaw-qwerty-left.bin
-make ergodox-infinity-fromtonrouge-dfu-util MASTER=right PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=no
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-colemak-jackdaw-qwerty-right.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=no .build/infinity-colemak-jackdaw-qwerty-left.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge MASTER=right PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=no .build/infinity-colemak-jackdaw-qwerty-right.bin
 
-make ergodox-infinity-fromtonrouge-dfu-util PROGRAMMER_COLEMAK_ENABLE=no AZERTY_OS_ENABLE=no
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-qwerty-jackdaw-qwerty-left.bin
-make ergodox-infinity-fromtonrouge-dfu-util MASTER=right PROGRAMMER_COLEMAK_ENABLE=no AZERTY_OS_ENABLE=no
-cp .build/ergodox_infinity_fromtonrouge.bin "$OLDPWD"/infinity/infinity-qwerty-jackdaw-qwerty-right.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge PROGRAMMER_COLEMAK_ENABLE=no AZERTY_OS_ENABLE=no .build/infinity-qwerty-jackdaw-qwerty-left.bin
+make keyboard=ergodox subproject=infinity keymap=fromtonrouge MASTER=right PROGRAMMER_COLEMAK_ENABLE=no AZERTY_OS_ENABLE=no .build/infinity-qwerty-jackdaw-qwerty-right.bin
+
+cp .build/infinity-*.bin "$OLDPWD"/infinity/
 
 cd "$OLDPWD"
