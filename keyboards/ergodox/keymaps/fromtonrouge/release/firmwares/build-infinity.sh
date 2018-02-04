7ec7ec940b835938e8b11f46e6e241ee28a23e8e
FromtonRouge <fromtonrouge@gmail.com>
2018-02-04 22:37:44 +0100

    Add a memo on how to flash an ergodox-infinity firmware

--- keyboards/ergodox/keymaps/fromtonrouge/release/firmwares/build-infinity.sh
+++ keyboards/ergodox/keymaps/fromtonrouge/release/firmwares/build-infinity.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Note: To flash the firmware just use: sudo dfu-util -D [firmware_file]
+# eg: sudo dfu-util -D infinity-colemak-jackdaw-azerty-right.bin 
 cd ../../../../../..
 rm -f .build/infinity-*.bin
 make keyboard=ergodox subproject=infinity keymap=fromtonrouge PROGRAMMER_COLEMAK_ENABLE=yes AZERTY_OS_ENABLE=yes .build/infinity-colemak-jackdaw-azerty-left.bin
