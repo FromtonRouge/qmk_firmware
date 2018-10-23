8efca730b2a106032c6fb652c7ccce791e287557
FromtonRouge <fromtonrouge@gmail.com>
2018-10-23 19:07:28 +0200

    Kaladrius: rules.mk updated

--- keyboards/handwired/kaladrius/teensy32/rules.mk
+++ keyboards/handwired/kaladrius/teensy32/rules.mk
@@ -1,9 +1,4 @@
-SRC = jackdaw/tables/tables.c \
-      jackdaw/tables/shelton_tables.c \
-      jackdaw/tables/user_tables.c \
-      jackdaw/stroke.c \
-      jackdaw/undo.c \
-      teensy32/matrix.c
+SRC += matrix.c
 
 FIRMWARE_FORMAT=hex
 
@@ -47,35 +42,6 @@ MCU = cortex-m4
 # ARM version, CORTEX-M0/M1 are 6, CORTEX-M3/M4/M7 are 7
 # I.e. 6 for Teensy LC; 7 for Teensy 3.x
 ARMV = 7
-
-# Processor frequency.
-#     This will define a symbol, F_CPU, in all source code files equal to the
-#     processor frequency in Hz. You can then use this symbol in your source code to
-#     calculate timings. Do NOT tack on a 'UL' at the end, this will be done
-#     automatically to create a 32-bit value in your source code.
-#
-#     This will be an integer division of F_USB below, as it is sourced by
-#     F_USB after it has run through any CPU prescalers. Note that this value
-#     does not *change* the processor frequency - it should merely be updated to
-#     reflect the processor speed set externally so that the code can use accurate
-#     software delays.
-F_CPU = 72000000
-
-# Input clock frequency.
-#     This will define a symbol, F_USB, in all source code files equal to the
-#     input clock frequency (before any prescaling is performed) in Hz. This value may
-#     differ from F_CPU if prescaling is used on the latter, and is required as the
-#     raw input clock is fed directly to the PLL sections of the AVR for high speed
-#     clock generation for the USB and other AVR subsections. Do NOT tack on a 'UL'
-#     at the end, this will be done automatically to create a 32-bit value in your
-#     source code.
-#
-#     If no clock division is performed on the input clock inside the AVR (via the
-#     CPU clock adjust registers or the clock division fuses), this will be equal to F_CPU.
-F_USB = $(F_CPU)
-
-# Interrupt driven control endpoint task(+60)
-OPT_DEFS += -DINTERRUPT_CONTROL_ENDPOINT
 
 # Bootloader selection
 #   Teensy       halfkay
