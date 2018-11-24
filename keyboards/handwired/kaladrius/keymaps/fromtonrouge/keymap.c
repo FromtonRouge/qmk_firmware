a5990809786eaab24d81677a376e344f5769cf17
FromtonRouge <fromtonrouge@gmail.com>
2018-11-24 15:41:23 +0100

    Kaladrius: Keymap updated

--- keyboards/handwired/kaladrius/keymaps/fromtonrouge/keymap.c
+++ keyboards/handwired/kaladrius/keymaps/fromtonrouge/keymap.c
@@ -73,11 +73,11 @@ const uint32_t PROGMEM g_steno_layout[MATRIX_ROWS][MATRIX_COLS] = LAYOUT(
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 {
     [L_STENO] = LAYOUT(
-        KC_F1,   KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_LGUI,      KC_TRNS, KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F11,     KC_F12, 
-        KC_LALT, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, KC_NO,        KC_NO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
-        KC_LSFT, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, KC_NO,        KC_NO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
-        KC_LCTL, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO,                      CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
-                              KC_NO,     KC_NO,                                                                      KC_NO,     KC_NO, 
+        KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_LGUI,      KC_TRNS, KC_F7,     KC_F8,     KC_F9,     KC_F10,     KC_F11,     KC_F12, 
+        KC_LALT,   CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, KC_ENT,       KC_NO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
+        CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, KC_ENT,       KC_NO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
+        CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO,                      CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, CKC_STENO, 
+                              KC_LSFT,     KC_LCTL,                                                                      KC_RCTL,     KC_RSFT, 
                                                                       CKC_STENO,        CKC_STENO, 
                                                 CKC_STENO, CKC_STENO, CKC_STENO,        CKC_STENO, CKC_STENO, CKC_STENO, 
                                                                       CKC_STENO,        CKC_STENO ),
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     [L_FN] = LAYOUT(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_TRNS,        KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,   KC_F12, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_ESC, KC_TRNS, KC_TRNS,  KC_TRNS, 
-        KC_LSFT, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RSFT, 
+        KC_LSFT, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_TRNS, RESET,          KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_RSFT, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, 
                       KC_TRNS, KC_TRNS,                                                                  KC_TRNS, KC_TRNS, 
                                                   KC_TRNS,        KC_TRNS, 
