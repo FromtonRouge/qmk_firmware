62beef6114215e553dc766f1a3f9057ce3be4189
FromtonRouge <fromtonrouge@gmail.com>
2019-06-10 20:47:14 +0200

    Kaladrius: matrix_is_on fix

--- keyboards/handwired/kaladrius/teensy32/matrix.c
+++ keyboards/handwired/kaladrius/teensy32/matrix.c
@@ -239,7 +239,7 @@ bool matrix_is_modified(void)
 
 bool matrix_is_on(uint8_t row, uint8_t col)
 {
-    return (matrix[row] & ((matrix_row_t)1<col));
+    return (matrix[row] & ((matrix_row_t)1<<col));
 }
 
 matrix_row_t matrix_get_row(uint8_t row)
